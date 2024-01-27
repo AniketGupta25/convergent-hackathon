b795033104ea317b68553d90c8862e8be70872e5
praneelseth <praneelseth@utexas.edu>
2024-01-27 16:24:59 -0600

    Working Upload

--- front-end/src/Home/FileUpload.js
+++ front-end/src/Home/FileUpload.js
@@ -5,7 +5,9 @@ import axios from 'axios'
 
 function FileUpload() {
 
-    const [file, setFile ] = useState(null);
+    const [ file, setFile ] = useState(null);
+    const [ progress, setProgress ] = useState({ started: false, pc: 0 });
+    const [ msg, setMsg ] = useState(null);
 
     function handleUpload() {
         if (!file) {
@@ -16,18 +18,35 @@ function FileUpload() {
         const fd = new FormData();
         fd.append('file', file);
 
-        axios.post('http://localhost:5000/url_route', fd, {
-            
+        setMsg("Uploading...");
+        setProgress(prevState => {
+            return {...prevState, started: true}
+        });
+        axios.post('http://localhost:5000/data', fd, {
+            onUploadProgress: (ProgressEvent) => { setProgress(prevState => {
+                return { ...prevState, pc: ProgressEvent.progress*100}
+            }) },
+            headers: {
+                "Custom-Header": "value",
+            }
         }).then(res => {
-            console.log(res.data)
+            setMsg("Upload successful")
+            console.log(res.data);
+
         })
-        .catch(err => console.error(err));
+        .catch(err => {
+            setMsg("Upload failed");
+            console.error(err);
+        });
     }
 
     return (
         <div className="demoItem">
             <input onChange={ (e) => { setFile(e.target.files[0]) } } type="file" className='file'/>
-            <button>Upload</button>
+            <button onClick={ handleUpload }>Upload</button>
+
+            { progress.started && <progress max="100" value={progress.pc}></progress> }
+            { msg && <span>{msg}</span> }
         </div>
     )
 }
