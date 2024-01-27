8cff431b29354df10ceda98558f2a61316ad2cc7
praneelseth <praneelseth@utexas.edu>
2024-01-27 16:00:40 -0600

    FileUpload

--- front-end/src/Home/FileUpload.js
+++ front-end/src/Home/FileUpload.js
@@ -4,30 +4,32 @@ import { useState } from 'react'
 import axios from 'axios'
 
 function FileUpload() {
-    uploadFile = async (e) => {
-        const file = e.target.files[0];
-    if (file != null) {
-        const data = new FormData();
-        data.append('file_from_react', file);
 
-        let response = await fetch('/url_route',
-        {
-            method: 'post',
-            body: data,
+    const [file, setFile ] = useState(null);
+
+    function handleUpload() {
+        if (!file) {
+            console.log("No file selected");
+            return;
         }
-        );
-        let res = await response.json();
-        if (res.status !== 1){
-        alert('Error uploading file');
-        }
+
+        const fd = new FormData();
+        fd.append('file', file);
+
+        axios.post('http://localhost:5000/url_route', fd, {
+            
+        }).then(res => {
+            console.log(res.data)
+        })
+        .catch(err => console.error(err));
     }
-};
 
-  return (
-    <div className="demoItem">
-        <input type="file" className='fileInput' onChange={this.uploadFile}/>
-    </div>
-  )
+    return (
+        <div className="demoItem">
+            <input onChange={ (e) => { setFile(e.target.files[0]) } } type="file" className='file'/>
+            <button>Upload</button>
+        </div>
+    )
 }
 
 export default FileUpload
